8360edf9aed4b19be1bd825e29abfe69ff8a7728
Densyakun <densyakun1128@gmail.com>
2022-06-02 03:29:02 +0000

    Change spaces to 2

--- tcp-websocket-tunnel.js
+++ tcp-websocket-tunnel.js
@@ -2,55 +2,55 @@ const net = require('net')
 const WebSocket = require('ws')
 
 exports.toWebSocket = (fromPort, toAddr) => {
-    const server = net.createServer(c => {
-        const ws = new WebSocket(toAddr)
+  const server = net.createServer(c => {
+    const ws = new WebSocket(toAddr)
 
-        ws.on('close', () => c.destroy())
+    ws.on('close', () => c.destroy())
 
-        ws.on('error', () => c.destroy())
+    ws.on('error', () => c.destroy())
 
-        c.on('end', () => ws.close(1000))
+    c.on('end', () => ws.close(1000))
 
-        c.on('error', () => ws.close(1000))
+    c.on('error', () => ws.close(1000))
 
-        ws.on('open', () => c.on('data', data => ws.send(data)))
+    ws.on('open', () => c.on('data', data => ws.send(data)))
 
-        ws.on('message', data => {
-            if (!c.destroyed)
-                c.write(data)
-        })
+    ws.on('message', data => {
+      if (!c.destroyed)
+        c.write(data)
     })
+  })
 
-    server.on('error', (err) => {
-        throw err
-    })
-    server.listen(fromPort, () => {
-        console.log(`TCP server listening on port ${fromPort}`)
-    })
+  server.on('error', (err) => {
+    throw err
+  })
+  server.listen(fromPort, () => {
+    console.log(`TCP server listening on port ${fromPort}`)
+  })
 
-    return server
+  return server
 }
 
 exports.toTCP = (fromPort, toPort, toHost) => {
-    const wss = new WebSocket.WebSocketServer({ port: fromPort })
+  const wss = new WebSocket.WebSocketServer({ port: fromPort })
 
-    wss.on('connection', ws => {
-        const client = net.connect(toPort, toHost)
+  wss.on('connection', ws => {
+    const client = net.connect(toPort, toHost)
 
-        client.on('end', () => ws.close(1000))
+    client.on('end', () => ws.close(1000))
 
-        client.on('error', () => ws.close(1000))
+    client.on('error', () => ws.close(1000))
 
-        ws.on('close', () => client.destroy())
+    ws.on('close', () => client.destroy())
 
-        ws.on('error', () => client.destroy())
+    ws.on('error', () => client.destroy())
 
-        ws.on('message', data => client.write(data))
+    ws.on('message', data => client.write(data))
 
-        client.on('data', data => ws.send(data))
-    })
+    client.on('data', data => ws.send(data))
+  })
 
-    console.log(`WebSocket server listening on port ${fromPort}`)
+  console.log(`WebSocket server listening on port ${fromPort}`)
 
-    return wss
+  return wss
 }
